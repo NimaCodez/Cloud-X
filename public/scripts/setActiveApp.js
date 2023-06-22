731cf264e6407213adcb4b1f754cfc3d226382a3
Nima <nimarahmati8par@gmail.com>
2023-06-22 22:34:42 +0330

    Added Select Location and Its JS for Selecting

--- public/scripts/setActiveApp.js
+++ public/scripts/setActiveApp.js
@@ -1,7 +1,12 @@
 const apps = document.querySelectorAll('.card')
+const locs = document.querySelectorAll('.loc-card')
 
 apps.forEach(app => {
     app.addEventListener('click', SetActiveAppOnClick)
+})
+
+locs.forEach(loc => {
+    loc.addEventListener('click', SetActiveLocOnClick)
 })
 
 function SetActiveAppOnClick(e) {
@@ -14,12 +19,32 @@ function SetActiveAppOnClick(e) {
         element.classList.add('active');
     }
 
-    else if (element.parentElement.classList.contains('card') ) {
+    else if (element.parentElement.classList.contains('card')) {
         element.parentElement.classList.add('active');
     }
 
     else if (element.parentElement.classList.contains('app-icon')) {
         element.parentElement.parentElement.classList.add('active');
+    }
+
+}
+
+function SetActiveLocOnClick(e) {
+    const element = e.target;
+
+    const activeElement = document.querySelector('.active-loc');
+    if (activeElement) activeElement.classList.remove('active-loc');
+
+    if (element.classList.contains('loc-card')) {
+        element.classList.add('active-loc');
+    }
+
+    else if (element.parentElement.classList.contains('loc-card') ) {
+        element.parentElement.classList.add('active-loc');
+    }
+
+    else if (element.parentElement.classList.contains('loc-icon')) {
+        element.parentElement.parentElement.classList.add('active-loc');
     }
 
 }
